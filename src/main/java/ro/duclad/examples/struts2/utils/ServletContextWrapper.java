fc13da5c10704e809dd9cc1bfd75189426ee4211
Claudiu Dumitrescu <claudiu.dumitrescu@gmail.com>
2015-08-24 16:30:50 +0200

    commit

--- src/main/java/ro/duclad/examples/struts2/utils/ServletContextWrapper.java
+++ src/main/java/ro/duclad/examples/struts2/utils/ServletContextWrapper.java
@@ -1,27 +1,26 @@
 package ro.duclad.examples.struts2.utils;
 
-import javax.servlet.ServletContext;
-
 import org.springframework.web.context.ServletContextAware;
+
+import javax.servlet.ServletContext;
 
 
 public class ServletContextWrapper implements ServletContextAware {
-	
-	private ServletContext context;
 
-	public ServletContextWrapper() {
-		;
-	}
-	
+    private ServletContext context;
+
+    public ServletContextWrapper() {
+        ;
+    }
+
 	/* -- VALUE ACCESSORS:
-	 */
-	
-	@Override
-	public void setServletContext(ServletContext context) {
-		this.context = context;
-	}
-	
-	public ServletContext getServletContext() {
-		return context;
-	}
+     */
+
+    public ServletContext getServletContext() {
+        return context;
+    }
+
+    public void setServletContext(ServletContext context) {
+        this.context = context;
+    }
 }
